d44aa24182ac678d461ddb0b7cb243fa0193a3dd
Alex Lenkoski <alex@nr.no>
2016-02-26 09:55:44 +0100

    Interpolation

--- scripts/replication.process.interpolation.R
+++ scripts/replication.process.interpolation.R
@@ -1,4 +1,18 @@
+rm(list = ls())
 
+load("returns.RData")
+load("cov.RData")
+
+Z.p <- matrix(unlist(l),ncol=3,byrow=TRUE)
+w.na <- which(apply(is.na(cov),1,"any"))
+Z <- matrix(NA,dim(cov)[1],5)
+Z[-w.na,] <- cbind(cov[-w.na,2:1],Z.p)
+Z[w.na,] <- cbind(cov[w.na,2:1], rbind(colMeans(Z.p),colMeans(Z.p)))
+
+colnames(Z) <- c("lon","lat", .025,.5,.975)
+rownames(Z) <- NULL
+write.csv(Z, file="map.txt",row.names=FALSE)
+##this all needs to be ncdf4.
 
 
 
