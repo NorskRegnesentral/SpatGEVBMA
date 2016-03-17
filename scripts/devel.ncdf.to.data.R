e383f33125a2e9ba746e50cfcc93a7f0ea37732a
Alex Lenkoski <alex@nr.no>
2016-03-17 14:35:12 +0100

    Data pipeline

---
+++ scripts/devel.ncdf.to.data.R
@@ -0,0 +1,44 @@
+rm(list = ls())
+
+library(SpatialGEVBMA)
+
+library(ncdf4io)
+
+setwd("~/NR/SpatGEV")
+
+l.all <- list.files("./inputs/til_Alex/",pattern = "*.nc")
+
+file <- paste("./inputs/til_Alex/",l.all[1],sep="")
+var.name <- "altCoord"
+proj.name="UTM_Zone_33"
+proj.att="projection"
+topdown=FALSE
+xdim.name=NULL
+ydim.name=NULL
+t=NULL
+
+a <- list()
+nm <- NULL
+for(i in 1:length(l.all))
+  {
+    a[[i]] <- nc4.matrix(paste("./inputs/til_Alex/",l.all[i],sep=""))
+    nm[i] <- strsplit(l.all[i],".",fixed=TRUE)[[1]][1]
+    X11()
+    image(a[[i]]$x,a[[i]]$y,a[[i]]$z,main=nm[i])
+  }
+
+##---- Sample some points ------
+w <- which(!is.na(a[[i]]$z))
+p <- NULL
+while(length(p) < 69 * 2)
+  {
+    x <- sample(1:length(a[[i]]$x),1)
+    y <- sample(1:length(a[[i]]$y),1)
+    if(!is.na(a[[i]]$z[x,y]))
+      {
+        p <- rbind(p,c(x,y))
+      }
+  }
+                
+
+
